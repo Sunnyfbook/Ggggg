f6e49290c948c2de960692883aa110c841381453
Sunny <142214739+Sunnyfbook@users.noreply.github.com>
2025-07-30 22:04:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,7 +12,7 @@ SESSION = environ.get('SESSION', 'Webavbot')
 API_ID = int(environ.get('API_ID', '1357592'))
 API_HASH = environ.get('API_HASH', '7a0b3567c813916acafaa69bf8989f12')
 BOT_TOKEN = environ.get('BOT_TOKEN', "8125185327:AAHqKXZxSg9GthfRX6VSg52aJ5Ik0bJzgAY")
-BOT_USERNAME = environ.get("BOT_USERNAME", 'CamGrabberInsta_bot') # without @ 
+BOT_USERNAME = environ.get("BOT_USERNAME", 'ViralMMs2035_bot') # without @ 
 
 # Admins, Channels & Users
 BIN_CHANNEL = int(environ.get("BIN_CHANNEL", '-1002679595778')) # admin your channel in stream 
