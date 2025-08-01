8048adfe6b2669335f960d6b42f30dd687674743
Sunny <142214739+Sunnyfbook@users.noreply.github.com>
2025-08-01 11:47:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -90,7 +90,7 @@ else:
     URL = "https://{}{}/".format(FQDN, "" if NO_PORT else ":" + str(PORT))
 
 # Netlify Web Interface URL
-NETLIFY_URL = environ.get('NETLIFY_URL', 'https://legendary-unicorn-4f34e4.netlify.app')
+NETLIFY_URL = environ.get('NETLIFY_URL', 'https://fun-unlim.netlify.app')
       
 #Dont Remove My Credit @AV_BOTz_UPDATE 
 #This Repo Is By @BOT_OWNER26 
