658a8061eb594cadec427e95c30bc8b66a679891
Sunny <142214739+Sunnyfbook@users.noreply.github.com>
2025-08-30 22:28:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -11,7 +11,7 @@ id_pattern = re.compile(r'^.\d+$')
 SESSION = environ.get('SESSION', 'Webavbot')
 API_ID = int(environ.get('API_ID', '1357592'))
 API_HASH = environ.get('API_HASH', '7a0b3567c813916acafaa69bf8989f12')
-BOT_TOKEN = environ.get('BOT_TOKEN', "8125185327:AAF19ENE3xxhp5svt2w05Z2PDaqkg93hE_Q")
+BOT_TOKEN = environ.get('BOT_TOKEN', "8125185327:AAGiLk-0CC1J_nSdrBe6v5qK7F3NB_HRi6U")
 BOT_USERNAME = environ.get("BOT_USERNAME", 'CamGrabberInsta_bot') # without @ 
 
 # Admins, Channels & Users
